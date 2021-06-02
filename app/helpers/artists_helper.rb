b950aec7a8f9acc0590edab1347bdd16c55627bf
Andréa Alencar <andreaflether@gmail.com>
2021-06-01 22:48:17 -0300

    Add link to Artist display page in AlbumPreview and Tracklist partials

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,7 @@
 module ArtistsHelper
+  def get_artists_link_collection(artists)
+    artists.map do |artist| 
+      link_to(artist.name, artist_path(artist.id))
+    end.join(', ').html_safe
+  end
 end
